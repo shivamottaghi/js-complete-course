6b285af1cad7045cfca5b13725e802203ee29864
shivamtg <shivamtg1376@gmail.com>
2022-05-05 10:39:57 +0200

    colors 03

--- 02-colors/03-change-bcg-three/script.js
+++ 02-colors/03-change-bcg-three/script.js
@@ -11,6 +11,7 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", () => {document.body.style.backgroundColor =
+        "#"+ Math.floor(Math.random()*16777215).toString(16);})
 
 })();
