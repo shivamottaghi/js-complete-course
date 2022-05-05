d2ed0b57052f583d30f5c2ff6c4602c8ce321320
shivamtg <shivamtg1376@gmail.com>
2022-05-05 16:49:17 +0200

    maths 01 - works better but the code is not good

--- 04-maths/01-calculator-one/script.js
+++ 04-maths/01-calculator-one/script.js
@@ -11,27 +11,43 @@
 
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
-    let op1 = document.getElementById("op-one").value;
-    parseInt(op1);
-    let op2 = document.getElementById("op-two").value;
-    parseInt(op2);
-    let output = 0;
+
     document.getElementById("addition").addEventListener("click", function() {
+        let op1 = document.getElementById("op-one").value;
+        parseInt(op1);
+        let op2 = document.getElementById("op-two").value;
+        parseInt(op2);
+        let output = 0;
         output = parseFloat(op1) + parseFloat(op2) ;
         window.alert(output);
     });
 
     document.getElementById("substraction").addEventListener("click", function() {
+        let op1 = document.getElementById("op-one").value;
+        parseInt(op1);
+        let op2 = document.getElementById("op-two").value;
+        parseInt(op2);
+        let output = 0;
         output = op1 - op2 ;
         window.alert(output);
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
+        let op1 = document.getElementById("op-one").value;
+        parseInt(op1);
+        let op2 = document.getElementById("op-two").value;
+        parseInt(op2);
+        let output = 0;
         output = op1 * op2 ;
         window.alert(output);
     });
 
     document.getElementById("division").addEventListener("click", function() {
+        let op1 = document.getElementById("op-one").value;
+        parseInt(op1);
+        let op2 = document.getElementById("op-two").value;
+        parseInt(op2);
+        let output = 0;
         output = op1 / op2 ;
         window.alert(output);
     });
