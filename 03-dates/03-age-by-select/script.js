cc25ff092a73c9900f239e3a34dfbe4fc98c04dd
shivamtg <shivamtg1376@gmail.com>
2022-05-05 12:19:12 +0200

    dates 03 not cmpltly correct

--- 03-dates/03-age-by-select/script.js
+++ 03-dates/03-age-by-select/script.js
@@ -12,5 +12,32 @@
 (function() {
 
     // your code here
+    document.getElementById("run").addEventListener("click", () =>
+    {
+        let day = document.getElementById("dob-day").value ;
+        let month = document.getElementById("dob-month").value - 1;
+        let year = document.getElementById("dob-year").value;
+
+        let d = new Date();
+        /*let today= d.getTime()*/
+        let today = d.getDate();
+        let thisMonth = d.getMonth();
+        console.log(d.getMonth())
+        let thisYear = d.getFullYear();
+        let ageYear = thisYear - year;
+        let ageMonth = thisMonth - month;
+        if (ageMonth < 0){
+            ageYear --;
+            ageMonth = 12 + ageMonth;
+        }
+        let ageDay = today - day ;
+        if (ageDay < 0 ){
+            ageMonth --;
+            ageDay = 30 + ageDay ;
+        }
+        let message =
+            "Yor Age is : " + ageYear + " Years and " + ageMonth + " Months and " + ageDay + " Days\n But it doesn't work completely right!";
+        window.alert(message);
+    })
 
 })();
