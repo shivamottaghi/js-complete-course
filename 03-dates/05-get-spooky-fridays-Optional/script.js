6ffe8635c2f096168c766ad5beb67b4e34d1b778
shivamtg <shivamtg1376@gmail.com>
2022-05-05 16:15:39 +0200

    dates 05

--- 03-dates/05-get-spooky-fridays-Optional/script.js
+++ 03-dates/05-get-spooky-fridays-Optional/script.js
@@ -11,6 +11,21 @@
 
 (function() {
 
-    // your code here
+    document.getElementById("run").addEventListener("click", ()=> {
+        let date = new Date();
+        let inputyear = document.getElementById("year").value;
+        date.setFullYear(inputyear);
+        date.setDate(13);
+        let months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+        let outputmonths = [];
+        for (let i = 0 ; i < 12 ; i++ ){
+            date.setMonth(i);
+            let weekday = date.getDay();
+            if (weekday == "5"){
+                outputmonths.push(months[i]);
+            }
+        }
+        console.log(outputmonths);
+    });
 
 })();
