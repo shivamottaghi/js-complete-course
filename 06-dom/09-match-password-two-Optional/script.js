3021604ecafa4cd576c86429ade6df4a8b714fa1
shivamtg <shivamtg1376@gmail.com>
2022-05-10 10:24:15 +0200

    dom 09

--- 06-dom/09-match-password-two-Optional/script.js
+++ 06-dom/09-match-password-two-Optional/script.js
@@ -11,6 +11,18 @@
 
 (function() {
 
-    // your code here
+    document.getElementById('run').addEventListener("click", () => {
+            let pass1 = document.getElementById('pass-one');
+            let pass2 = document.getElementById('pass-two');
+            let orgstyle = pass1.style;
+            if (pass1.value != pass2.value) {
+                pass1.classList.add('error') ;
+                pass2.classList.add('error') ;
+            } else {
+                pass1.classList.remove('error');
+                pass2.classList.remove('error');
+            }
+
+        });
 
 })();
