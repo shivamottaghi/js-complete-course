492233c1b12cbc9f45d9798a7e761b6bbe8743a7
shivamtg <shivamtg1376@gmail.com>
2022-05-10 09:57:26 +0200

    dom 08

--- 06-dom/08-match-password-one/script.js
+++ 06-dom/08-match-password-one/script.js
@@ -10,7 +10,20 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById('run').addEventListener("click",()=>{
+        let warning = false;
+        let pass1 = document.getElementById('pass-one');
+        let pass2 = document.getElementById('pass-two');
+        let orgstyle = pass1.style;
+        if ( pass1.value != pass2.value ){
+            pass1.style.border = '3px solid red';
+            pass2.style.border = '3px solid red';
+        }else {
+            pass1.style = orgstyle;
+            pass2.style = orgstyle;
+        }
 
-    // your code here
+    });
+
 
 })();
