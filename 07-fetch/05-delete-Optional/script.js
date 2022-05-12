b60ffa7083310441b74bbf67cfb143254973ec4d
shivamtg <shivamtg1376@gmail.com>
2022-05-12 15:45:41 +0200

    fetch 05

--- 07-fetch/05-delete-Optional/script.js
+++ 07-fetch/05-delete-Optional/script.js
@@ -10,5 +10,16 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    fetch('../../_shared/api.json')
+        .then(data => data.json())
+        .then(jsobj => {
+            let heroes = jsobj.heroes;
+            console.log(heroes);
+            document.getElementById('run').addEventListener('click', () =>{
+                let delid = document.getElementById('hero-id').value;
+                let found = heroes.find(hero => hero.id == delid );
+                heroes.splice(heroes.indexOf(found) , 1);
+                console.log(heroes);
+            })
+        })
 })();
