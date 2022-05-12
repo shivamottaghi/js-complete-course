2513afc01ee60dcfcb6132ba1d9025ee3d7ac29a
shivamtg <shivamtg1376@gmail.com>
2022-05-12 14:51:49 +0200

    fetch 04

--- 07-fetch/04-add-Optional/script.js
+++ 07-fetch/04-add-Optional/script.js
@@ -10,5 +10,47 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    fetch('../../_shared/api.json')
+        .then(data => data.json())
+        .then(jsobj => {
+            let heroes = jsobj.heroes;
+            document.getElementById('run').addEventListener('click' ,()=>{
+                let name = document.getElementById('hero-name').value;
+                let alterego = document.getElementById('hero-alter-ego').value;
+                let powers = document.getElementById('hero-powers').value.split(', ');
+                let myid = heroes.length + 1;
+                let myhero = {id: myid, name: `${name}`, alerEgo: `${alterego}`, abilities: powers};
+                heroes.push(myhero);
+                console.log(heroes);
+            } )
+
+
+        })
+
+   /* document.getElementById('run').addEventListener('click', ()=>{
+        let name = document.getElementById('hero-name').value;
+        let alterego = document.getElementById('hero-alter-ego').value;
+        let powers = document.getElementById('hero-powers').value.split(', ');
+        console.log(powers);
+        async function getData(){
+            let data = await fetch('../../_shared/api.json');
+            let jsobj = await data.json();
+            return jsobj.heroes;
+        }
+        async function addHero(){
+            let heroes = await getData();
+            let myid = heroes.length + 1;
+            let myhero = {id: myid, name: `${name}`, alerEgo: `${alterego}`, abilities: powers};
+            heroes.push(myhero);
+            return heroes;
+        }
+
+        async function displayHeroes(){
+            let heroes = await addHero();
+            console.log(heroes);
+        }
+
+        displayHeroes();
+    })*/
 })();
