a32f4b860c80533cba89ef0b8ef71999dff9cdef
shivamtg <shivamtg1376@gmail.com>
2022-05-12 11:13:17 +0200

    fetch 03

--- 07-fetch/03-list-to-template-Optional/script.js
+++ 07-fetch/03-list-to-template-Optional/script.js
@@ -10,5 +10,31 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    let templ = document.getElementById('tpl-hero');
+
+    document.getElementById('run').addEventListener('click' ,() => {
+        /*----Async function to fetch the data-----*/
+        async function getData(){
+            let data = await fetch('../../_shared/api.json');
+            let jsobj = await data.json();
+            return jsobj.heroes;
+        }
+        /*----Async function to display the data----*/
+        async function displayData(){
+            let heroes = await getData();
+            heroes.forEach(hero => {
+                console.log(hero);
+                let clone = templ.content.cloneNode(true);
+                let name = clone.querySelector('.name');
+                let ego = clone.querySelector('.alter-ego');
+                let powers = clone.querySelector('.powers');
+                let target = document.getElementById('target');
+                name.innerText = hero.name;
+                ego.innerText = hero.alterEgo;
+                powers.innerText = hero.abilities;
+                target.appendChild(clone);
+            })
+        }
+        displayData();
+    })
 })();
